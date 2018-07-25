4d56ccd906f2a294fd4a7cd548585236aa9ff535
ammyhran <ammyhran@gmail.com>
2018-07-25 21:15:42 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -15,25 +15,35 @@
 # end
 
 
-def create_olympics_hash
-  places_array = [:"Sydney", :"Athens", :"Beijing", :"London"]
-  years_array = [2000, 2004, 2008, 2012]
 
-  places_years = 0  
-  create_olympics_hash = {}
 
-  places_array.each do |places|
-    create_olympics_hash[places] = years_array[places_years]
-    places_years += 1
-  end
-  return create_olympics_hash
-end 
 
-def iterate_through_hash
- create_olympics_hash.each do |locations, date|
-  puts "The #{locations} summer olympics took place in #{date}."
-  end
-end 
+
+# def create_olympics_hash
+#   places_array = [:"Sydney", :"Athens", :"Beijing", :"London"]
+#   years_array = [2000, 2004, 2008, 2012]
+
+#   places_years = 0  
+#   create_olympics_hash = {}
+
+#   places_array.each do |places|
+#     create_olympics_hash[places] = years_array[places_years]
+#     places_years += 1
+#   end
+  
+#   return create_olympics_hash
+# end 
+
+# def iterate_through_hash
+# create_olympics_hash.each do |locations, date|
+#   puts "The #{locations} summer olympics took place in #{date}."
+#   end
+# end 
+  
+  
+  
+  
+  
   
 iterate_through_hash  
   
@@ -50,3 +60,15 @@ iterate_through_hash
 #   # Implement this method so that it converts the keys of the hash created in add_a_key_value_pair
 #   # into uppercased Strings, creates a new array with these values, and then puts each upcase key in the CLI
 # end
+
+
+
+create_olympics_hash = {
+
+
+
+
+
+
+
+
